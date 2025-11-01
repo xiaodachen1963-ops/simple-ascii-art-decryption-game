fd6f9e3ed74d28afbde888264337b55009ef7bac
xiaodachen1963-ops <xiaodachen1963@gmail.com>
2025-11-01 16:55:20 +0800

    input_procces.c

--- ascii_art_game/input_procces.c
+++ ascii_art_game/input_procces.c
@@ -1,19 +1,10 @@
 #include "space_station.h"
 
 int up(int current_room,char rooms[2][rows][cols],int player_location[2]){
-    /*player_location[1] = player_location[1] - 1;
+    player_location[1] = player_location[1] - 1;
     if(current_room == 1){
         rooms[0][player_location[1]+1][player_location[0]] = ' ';
         rooms[0][player_location[1]][player_location[0]] = 'a';
-    }
-    return 0;*/
-    if (player_location[1] > 0) {  // 确保不会越界
-        // 清除当前位置
-        rooms[current_room][player_location[1]][player_location[0]] = ' ';
-        // 更新玩家位置
-        player_location[1] -= 1;
-        // 设置新的玩家位置
-        rooms[current_room][player_location[1]][player_location[0]] = 'a';
     }
     return 0;
 }
@@ -25,4 +16,5 @@ int left(int current_room,char rooms[2][rows][cols],int player_location[2]){
 }
 int right(int current_room,char rooms[2][rows][cols],int player_location[2]){
     return 0;
+
 }
